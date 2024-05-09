0033b5936f723e0ca6ffa8bb487d9adaf76696d0
Sakthivel P <sakthivel.p@ind.xebiadigital.com>
2024-05-09 12:08:26 +0530

    smile pls

--- backend/myenv/ecomproject/ecomapp/views.py
+++ backend/myenv/ecomproject/ecomapp/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,permission_classes
+from rest_framework.permissions import IsAuthenticated,IsAdminUser
 # from .products import products
 from .models import Products
-from .serializer import ProductSerializer
+from .serializer import ProductSerializer,UserSerializer,UserSerializerWithToken
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth.models import User
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -20,3 +24,36 @@ def getProduct(request,pk):
     product=Products.objects.get(_id=pk)
     serializer=ProductSerializer(product,many=False)
     return Response(serializer.data)
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self,attrs):
+        data=super().validate(attrs)
+        serializer=UserSerializerWithToken(self.user).data
+        for k,v in serializer.items():
+            data[k]=v
+        return data
+    
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class=MyTokenObtainPairSerializer
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getUserProfiles(request):
+    user=request.user
+    serializer=UserSerializer(user,many=False)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users=User.objects.all()
+    serializer=UserSerializer(users,many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def registerUser(request):
+    data=request.data
+    try:
+        pass
+    except:
+        pass
